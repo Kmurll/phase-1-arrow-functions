e2f94d5d4dfc245499d1ca2b62f8f787dc191f1c
Kelvin Muli Mue <kelvinmue@gmail.com>
2023-07-11 11:37:43 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+let num1 = 2000;
+let num2 = 100;
+
+const divide = function() {
+  return num1 / num2;
+};
+
+console.log(divide()); // Output: 20
+
+const square = (num) => {
+    return num * num;
+  };
+  
+  console.log(square(2)); // Output: 4
+
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+  
+  console.log(add(2, 3)); // Output: 5
+  
+  
+
+
+
